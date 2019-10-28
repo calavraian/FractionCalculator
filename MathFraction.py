ffb814aa237a9519afde98b24c52fc975f1b8984
Juan Carlos Araujo Borrego <jcborre@digono.com>
2019-10-28 10:58:50 -0700

    Class added to represent fraction objects

--- MathFraction.py
+++ MathFraction.py
@@ -4,6 +4,15 @@ Created on Oct 28, 2019
 @author: calavraian
 '''
 
+class Fraction():
+    def __init__(self, whole, numerator, denominator):
+        self.whole = whole
+        self.numerator = numerator
+        self.denominator = abs(denominator)
+    
+    def __str__(self):
+        return "{}_{}/{}".format(self.whole, self.numerator, self.denominator)
+
 def main():
     opElements = list(filter(lambda x: x.strip(), input("Operation> ").split(" ")))
     print(opElements)
